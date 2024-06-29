f8a998c9cb32ae252bdef7745ace12f0441f0848
Deadman <47807486+Deaadman@users.noreply.github.com>
2024-06-29 11:18:45 +0930

    Couple game breaking bugs fixed, updated for 2.30

--- Source/Utilities/Logging.cs
+++ Source/Utilities/Logging.cs
@@ -4,14 +4,10 @@ namespace AdaptiveArsenal.Utilities;
 
 internal static class Logging
 {
-    internal static void LogStarter()                                                     => Melon<Mod>.Logger.Msg($"Mod loaded with v{Properties.BuildInfo.Version}");
-    internal static void LogSeparator(params object[] parameters)                         => Melon<Mod>.Logger.Msg("==============================================================================", parameters);
-    internal static void LogIntraSeparator(string message, params object[] parameters)    => Melon<Mod>.Logger.Msg($"=========================   {message}   =========================", parameters);
-
-    internal static void Log(string message, params object[] parameters)                  => Melon<Mod>.Logger.Msg(message, parameters);
-    internal static void LogDebug(string message, params object[] parameters)             => Melon<Mod>.Logger.Msg($"[DEBUG] {message}", parameters);
-    internal static void LogWarning(string message, params object[] parameters)           => Melon<Mod>.Logger.Warning(message, parameters);
-    internal static void LogError(string message, params object[] parameters)             => Melon<Mod>.Logger.Error(message, parameters);
+    internal static void Log(string message, params object[] parameters) => Melon<Mod>.Logger.Msg(message, parameters);
+    internal static void LogDebug(string message, params object[] parameters) => Melon<Mod>.Logger.Msg($"[DEBUG] {message}", parameters);
+    internal static void LogWarning(string message, params object[] parameters) => Melon<Mod>.Logger.Warning(message, parameters);
+    internal static void LogError(string message, params object[] parameters) => Melon<Mod>.Logger.Error(message, parameters);
     internal static void LogException(string message, Exception exception, params object[] parameters)
     {
         StringBuilder sb = new();
